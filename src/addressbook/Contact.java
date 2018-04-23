f5ab656a1040658d06f40cce739bf18a4c2f4a4b
bwielk <bwielk@gmail.com>
2018-04-23 19:58:52 +0200

    Getters work

--- src/addressbook/Contact.java
+++ src/addressbook/Contact.java
@@ -2,5 +2,39 @@ package addressbook;
 
 public class Contact {
 
+    private String name;
+    private String surname;
+    private String email;
+    private String skypeName;
+    private String displayedName;
+
+    public Contact(String name, String surname, String email, String skypeName, String displayedName){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.skypeName = skypeName;
+        this.displayedName = displayedName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getSurname(){
+        return this.surname;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getSkypeName(){
+        return this.skypeName;
+    }
+
+    public String getDisplayedName(){
+        return this.displayedName;
+    }
+
 }
 
--- tests/addressbook/ContactTest.java
+++ tests/addressbook/ContactTest.java
@@ -12,13 +12,32 @@ public class ContactTest {
 
     @Before
     public void before(){
-        contact1 = new Contact();
-        contact2 = new Contact();
+        contact1 = new Contact("Eva","Messica","em@em.com","evessica","Eva");
+        contact2 = new Contact("James", "Deen", "jd@jd.com", "jdeen", "James");
     }
 
     @Test
     public void classContactExists(){
         assertEquals(Contact.class, contact1.getClass());
         assertEquals(Contact.class, contact2.getClass());
+    }
+
+    @Test
+    public void contactContainsAllTheDetails_gettersWork(){
+        //contact has name
+        assertEquals("Eva", contact1.getName());
+        assertEquals("James", contact2.getName());
+        //contact has surname
+        assertEquals("Messica", contact1.getSurname());
+        assertEquals("Deen", contact2.getSurname());
+        //contact has email
+        assertEquals("em@em.com", contact1.getEmail());
+        assertEquals("jd@jd.com", contact2.getEmail());
+        //contact has skype name
+        assertEquals("evessica", contact1.getSkypeName());
+        assertEquals("jdeen", contact2.getSkypeName());
+        //contact has a displayed name
+        assertEquals("Eva", contact1.getDisplayedName());
+        assertEquals("James", contact2.getDisplayedName());
     }
 }
