4a89bb75d610896a8cf0d51638ddca4a2fe8f5f8
bwielk <bwielk@gmail.com>
2018-04-25 19:35:27 +0200

    contacts.txt file is created and stores items data

--- src/addressbook/Contacts.java
+++ src/addressbook/Contacts.java
@@ -1,5 +1,8 @@
 package addressbook;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Contacts {
@@ -7,7 +10,7 @@ public class Contacts {
     private static LinkedHashMap<String, Contact> contacts = new LinkedHashMap<String, Contact>();
     private static Contact newContact;
 
-    public static void main(String args[]) throws Contact.WrongEmailFormatException, Contact.WrongPhoneNumberFormatException {
+    public static void main(String args[]) throws Contact.WrongEmailFormatException, Contact.WrongPhoneNumberFormatException, IOException {
 
         newContact = new Contact("Amy", "Winehouse", "amywinehouse@gmail.co", "903432123", "amyw", "Amy");
         contacts.put(newContact.getDisplayedName(), newContact);
@@ -18,6 +21,16 @@ public class Contacts {
         newContact = new Contact("Jenny", "Packham", "jenny@jennypack.com", "777623231", "jennypack", "Jenny");
         contacts.put(newContact.getDisplayedName(), newContact);
 
-        System.out.println(contacts.size());
+        System.out.println(contacts.toString());
+
+        try(BufferedWriter contactsFile = new BufferedWriter(new FileWriter("contacts.txt"))){
+            for(Contact contact : contacts.values()){
+                contactsFile.write(contact.getName() + "\t"
+                        + contact.getSurname() + "\t"
+                        + contact.getPhoneNum() + "\t"
+                        + contact.getEmail() + "\t"
+                        + contact.getSkypeName() + "\n");
+            }
+        }
     }
 }
