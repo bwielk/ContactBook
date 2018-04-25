10880f541eed28733a2819f621d8d7ff9b2f4cd4
bwielk <bwielk@gmail.com>
2018-04-25 21:03:05 +0200

    Tidied up the code

--- src/addressbook/Contacts.java
+++ src/addressbook/Contacts.java
@@ -6,19 +6,8 @@ import java.util.*;
 public class Contacts {
 
     private static LinkedHashMap<String, Contact> contacts = new LinkedHashMap<String, Contact>();
-    private static Contact newContact;
-
-    public static void main(String args[]) throws IOException, Contact.WrongEmailFormatException, Contact.WrongPhoneNumberFormatException {
-
-//        newContact = new Contact("Amy", "Winehouse", "amywinehouse@gmail.co", "903432123", "amyw", "Amy");
-//        contacts.put(newContact.getDisplayedName(), newContact);
-//        newContact = new Contact("Kylie", "Minogue", "kyliekylie@kylie.eu", "123412123", "kkkylie", "Kylie");
-//        contacts.put(newContact.getDisplayedName(), newContact);
-//        newContact = new Contact("Maddie", "Donna", "mad@donna.co", "900902313", "madonna123", "Madonna");
-//        contacts.put(newContact.getDisplayedName(), newContact);
-//        newContact = new Contact("Jenny", "Packham", "jenny@jennypack.com", "777623231", "jennypack", "Jenny");
-//        contacts.put(newContact.getDisplayedName(), newContact);
-        //System.out.println(contacts.toString());
+    
+    public static void main(String args[]) throws IOException{
 
         try(BufferedWriter contactsFile = new BufferedWriter(new FileWriter("contacts.txt"))){
             for(Contact contact : contacts.values()){
@@ -56,7 +45,7 @@ public class Contacts {
 
                 String displayedName = scanner.nextLine();
                 newContact = new Contact(name, surname, email, phoneNum, skype, displayedName);
-                System.out.println(newContact.toString());
+                System.out.println("ADDED " + newContact.toString());
                 contacts.put(displayedName, newContact);
             }
         } catch (FileNotFoundException e) {
