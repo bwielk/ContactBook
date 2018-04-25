4987ba3c572eb6a746bc9a5869b4557a169f600e
bwielk <bwielk@gmail.com>
2018-04-25 20:30:24 +0200

    Implementing the BufferedReader

--- src/addressbook/Contacts.java
+++ src/addressbook/Contacts.java
@@ -1,8 +1,6 @@
 package addressbook;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Contacts {
@@ -10,7 +8,7 @@ public class Contacts {
     private static LinkedHashMap<String, Contact> contacts = new LinkedHashMap<String, Contact>();
     private static Contact newContact;
 
-    public static void main(String args[]) throws Contact.WrongEmailFormatException, Contact.WrongPhoneNumberFormatException, IOException {
+    public static void main(String args[]) throws IOException, Contact.WrongEmailFormatException, Contact.WrongPhoneNumberFormatException {
 
         newContact = new Contact("Amy", "Winehouse", "amywinehouse@gmail.co", "903432123", "amyw", "Amy");
         contacts.put(newContact.getDisplayedName(), newContact);
@@ -20,18 +18,50 @@ public class Contacts {
         contacts.put(newContact.getDisplayedName(), newContact);
         newContact = new Contact("Jenny", "Packham", "jenny@jennypack.com", "777623231", "jennypack", "Jenny");
         contacts.put(newContact.getDisplayedName(), newContact);
-
         System.out.println(contacts.toString());
 
         try(BufferedWriter contactsFile = new BufferedWriter(new FileWriter("contacts.txt"))){
             for(Contact contact : contacts.values()){
-                contactsFile.write(contact.getName() + "\t"
+                contactsFile.write(
+                        contact.getName() + "\t"
                         + contact.getSurname() + "\t"
-                        + contact.getPhoneNum() + "\t"
                         + contact.getEmail() + "\t"
+                        + contact.getPhoneNum() + "\t"
                         + contact.getSkypeName() + "\t"
                         + contact.getDisplayedName() + "\n");
             }
+        }
+    }
+
+    static {
+
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("contacts.txt")))){
+            scanner.useDelimiter("\t");
+            while(scanner.hasNextLine()) {
+                String name = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                String surname = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                String email = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                String phoneNum = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                String skype = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                String displayedName = scanner.next();
+                contacts.put(displayedName, new Contact(name, surname, email, phoneNum, skype, displayedName));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Contact.WrongEmailFormatException e) { //from new Contact creation
+            e.printStackTrace();
+        } catch (Contact.WrongPhoneNumberFormatException e) { // from new Contact creation
+            e.printStackTrace();
         }
     }
 }
